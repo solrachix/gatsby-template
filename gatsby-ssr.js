d7cad9b447c84c75a7462dbbd431cb2be8b4d28a
Carlos Miguel <carlos.miguel.oliveira.17@gmail.com>
2020-04-07 15:20:54 -0300

    retoques

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,1 +1,18 @@
+/**
+ * render server side
+ */
 export { wrapPageElement } from './gatsby/wrapPageElement';
+
+/**
+ * headComponents       : Setar algo no HEAD
+ * preBodyComponents    : Setar algo depois de abrir a tag Body
+ * setPostBodyComponents: Setar algo antes de fechar a tag Body
+ */
+
+// export const onRenderBody = ({
+//   setPostBodyComponents,
+// }) => {
+//   setPostBodyComponents([
+//     <script type="text/javascript" src=""/>
+//   ])
+// }
