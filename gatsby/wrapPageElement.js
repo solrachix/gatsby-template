a4b83ac7a8f848d54a4d3f6ddf65976ad1df6b08
Carlos Miguel <carlos.miguel.17@gmail.com>
2020-04-08 16:42:14 -0300

    improving syntax

--- gatsby/wrapPageElement.js
+++ gatsby/wrapPageElement.js
@@ -1,5 +1,5 @@
 const React = require('react');
-const Layout = require('../src/components/layout').default;
+const Layout = require('../src/components/Layout').default;
 
 exports.wrapPageElement = ({ element, props }) => {
   // props provide same data to Layout as Page element will get
